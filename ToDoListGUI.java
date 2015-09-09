4f9a81c475cc47c6e5adc1f3658d646040592908
Christina Mak <cymak@ucsd.edu>
2015-09-09 14:42:34 -0700

    Neatened design of GUI. Factored out magic numbers.

--- ToDoListGUI.java
+++ ToDoListGUI.java
@@ -17,7 +17,11 @@ import javafx.geometry.*;
 public class ToDoListGUI extends Application
 {
     private static final double WIDTH = 400;
-    private static final double HEIGHT = 800;
+    private static final double HEIGHT = 400;
+    private static final double INSET_SIZE = 50;
+    private static final double VERT_SP = 10;
+    private static final double BTN_BOX_SP = 5;
+    private static final double BTN_WIDTH = 57;
 
     private ToDoList list = new ToDoList();
 
@@ -26,24 +30,27 @@ public class ToDoListGUI extends Application
     {
         // the pane
         GridPane pane = new GridPane();
-        pane.setAlignment(Pos.BOTTOM_CENTER);
-
+        pane.setAlignment(Pos.CENTER);
+        
         // the text of the list
-        VBox listBox = new VBox(10);
+        VBox listBox = new VBox(VERT_SP);
         listBox.setAlignment(Pos.CENTER);
-        listBox.setPadding(new Insets(50));
-//        Text listText = new Text("List text placeholder");
+        listBox.setPadding(new Insets(INSET_SIZE));
         TextArea listArea = new TextArea("List area placeholder");
         listArea.setEditable(false);
         listBox.getChildren().add(listArea);
 
-        // the buttons
-        VBox buttonBox = new VBox(10);
-        buttonBox.setAlignment(Pos.BOTTOM_CENTER);
-        buttonBox.setPadding(new Insets(50));
+        // the hbox for add field and button
         Button addButton = new Button("Add");
+        TextField addField = new TextField(); // field to let user add item to list
+        HBox addBox = new HBox(BTN_BOX_SP);
+        initBtnBox(addBox, addField, addButton, listBox);
+
+        // the hbox for delete button
         Button deleteButton = new Button("Delete");
-        listBox.getChildren().addAll(addButton, deleteButton);
+        TextField deleteField = new TextField();  // field to let user delete item from list
+        HBox deleteBox = new HBox(BTN_BOX_SP);
+        initBtnBox(deleteBox, deleteField, deleteButton, listBox);
 
 //        // make the add button the default button when ENTER is pressed
 //        addButton.setDefaultButton(true);
@@ -51,10 +58,6 @@ public class ToDoListGUI extends Application
         addButton.setOnAction(new AddHandler());
 
         pane.add(listBox, 0, 0);
-        //pane.add(buttonBox, 5, 5);
-
-        //pane.add(addButton, 200, 200);
-        //pane.add(deleteButton, 200, 600);
 
         // the scene and the stage
         Scene scene = new Scene(pane, WIDTH, HEIGHT);
@@ -81,6 +84,16 @@ public class ToDoListGUI extends Application
     private void updateListText(Text listText, String listString)
     {
         listText.setText(listString);
+    }
+
+    private void initBtnBox(HBox hbox, TextField field, Button button, VBox vbox)
+    {
+        button.setMinWidth(BTN_WIDTH);
+        hbox.setAlignment(Pos.CENTER_RIGHT);
+        hbox.getChildren().addAll(field, button);
+        //hbox.setMinWidth(vbox.getWidth() - BTN_BOX_SP - BTN_WIDTH);
+        field.setMinWidth(300); //TODO tune width
+        vbox.getChildren().add(hbox);
     }
 
 //    public static void main(String[] args)
